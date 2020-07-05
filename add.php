7bdd3a6616ea9fd74e331bec4af6c9f4204c1ebc
lolla <aliaezz42@gmail.com>
2020-07-05 18:19:31 +0200

    Add page general structure

---
+++ add.php
@@ -0,0 +1,41 @@
+<?php
+  include "init.php";
+?>
+
+<?php
+  include "inc/logout.php";
+?>
+
+  <!-- add section structure -->
+  <!-- Save & Returne -->
+<?php
+  include "inc/save_return.php";
+?>
+
+  <!-- add new recurring  -->
+<?php
+  include "inc/sections/add/Add_recurring.php";
+?>
+
+  <!-- add new customer structure -->
+<?php
+  include "inc/sections/add/Add_customer.php";
+?>
+
+    <!-- add new product structure  -->
+<?php
+  include "inc/sections/add/Add_product.php";
+?>
+  <!-- add new Order structure  -->
+<?php
+  include "inc/sections/add/Add_order.php";
+?>
+
+  <!-- add new supplier structure  -->
+<?php
+  include "inc/sections/add/Add_supplier.php";
+?>
+
+<?php
+  include "inc/htmlend.php";
+?>
